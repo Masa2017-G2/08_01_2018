33ff614e088301d09598c2e90c3efb5b39555ca0
Gregory Sheygam <sheygam@gmail.com>
2018-01-08 23:09:25 +0200

    first commit

---
+++ app/src/main/java/com/sheygam/masa_2017_g2_08_01_18/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sheygam.masa_2017_g2_08_01_18;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
